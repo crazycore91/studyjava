8bdf73798cee97af67380767502b3b7fe9dd5ea5
gachkovskya <gachkovskya@0.0.0.97>
2017-04-05 17:38:08 +0300

    first commit

---
+++ src/dztask1stlesson/tasks1.java
@@ -0,0 +1,28 @@
+package dztask1stlesson;
+
+public class tasks1 {
+
+	public static void main(String[] args) {
+/*int A= 5;
+int B= 7;
+int C = A;
+A=B;
+B=C;
+A = 7;
+B = 5;
+System.out.println(A);
+System.out.println(B);*/
+	
+	//7tsk
+		int A= 5;
+		int B= 7;
+		int C= 9;
+		int Q = A;
+		B=A;
+		A=C;
+		System.out.println(A);
+		System.out.println(B);
+		System.out.println(C);
+	}
+
+}
