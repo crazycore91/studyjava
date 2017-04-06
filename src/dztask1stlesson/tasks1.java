cb8633cc694bef157dc08f61710f524a1dd42019
gachkovskya <gachkovskya@0.0.0.97>
2017-04-06 17:45:20 +0300

    comm2

--- src/dztask1stlesson/tasks1.java
+++ src/dztask1stlesson/tasks1.java
@@ -3,28 +3,51 @@ package dztask1stlesson;
 public class tasks1 {
 
 	public static void main(String[] args) {
-/*int A= 5;
-int B= 7;
-int C = A;
-A=B;
-B=C;
-A = 7;
-B = 5;
-System.out.println(A);
-System.out.println(B);*/
-	//666
-	//7tsk
-		int A= 4;
-		int B= 6;
-		int C= 8;
-		int Q = A;
-		Q=A;
-		A=C;
-		C=B;
-		B=Q;
+		/*int A= 5;
+		int B= 7;
+		int C = A;
+		A=B;
+		B=C;
+		A = 7;
+		B = 5;
 		System.out.println(A);
-		System.out.println(B);
-		System.out.println(C);
+		System.out.println(B);*/
+			
+			//7tsk
+		/*		int A= 4;
+				int B= 6;
+				int C= 8;
+				int Q = A;
+				Q=A;
+				A=C;
+				C=B;
+				B=Q;
+				System.out.println(A);
+				System.out.println(B);
+				System.out.println(C);*/
+				
+				
+			//task4.1.1
+				//1st
+				
+			/*int A = -4;
+			boolean a1 = A >= 0;
+			System.out.println(a1);*/
+			
+				//2st
+			
+				/*int A = 4;	
+				boolean a1 = (A%1)==0;
+				System.out.println(a1);*/
+				
+				//3st
+				//Даны три целых числа: A, B, C. Проверить истинность высказывания: «Справедливо двойное неравенство A < B < C».
+				int A= 21;
+				int B= 5;
+				int C= 8;
+				boolean a1 = (A < B)==true;
+				boolean b1 = (B < C)==true;
+				System.out.println(a1);
 	}
 
 }
