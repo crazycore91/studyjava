2aaad2440ffa3545c642e177237d852247aa8b8d
gachkovskya <gachkovskya@0.0.0.97>
2017-04-07 09:48:28 +0300

    newcomm

--- src/dztask1stlesson/tasks1.java
+++ src/dztask1stlesson/tasks1.java
@@ -41,13 +41,118 @@ public class tasks1 {
 				System.out.println(a1);*/
 				
 				//3st
-				//Даны три целых числа: A, B, C. Проверить истинность высказывания: «Справедливо двойное неравенство A < B < C».
-				int A= 21;
-				int B= 5;
-				int C= 8;
-				boolean a1 = (A < B)==true;
-				boolean b1 = (B < C)==true;
-				System.out.println(a1);
-	}
+	    //Даны три целых числа: A, B, C. Проверить истинность высказывания: «Справедливо двойное неравенство A < B < C».
+		/*int A= 21;
+		int B= 5;
+		int C= 8;
+		boolean a1 = (A < B) && (B < C);
+		System.out.println(a1);*/
 
-}
+
+		//4d
+		/*int A= 13;
+		int B= -5;
+		int C= 8;
+		boolean a1 = (A > 0) || (B > 0 )|| (C > 0) ;
+		System.out.println(a1);*/
+
+
+		//5
+		/*int A= 3;
+		int B= -7;
+		int C= 8;
+		boolean a1 =((A > 0) && (B < 0 ) && (C < 0)) || 
+		((A < 0) && (B > 0 ) && (C < 0)) || 
+		((A < 0) && (B < 0 ) && (C > 0));
+		System.out.println(a1);*/
+
+		//6
+
+		/* int A= 13;
+		boolean a1 = ((A>99) && (A<1000)) && (A%2)!=0;
+		System.out.println(a1);
+		*/
+
+		//7
+
+		/* int x = 5; 
+		int y = 5; 
+		int x1 = 5; 
+		int x2 = 5; 
+		int y1 = 7; 
+		int y2 = 7;
+		double c = Math.abs(x1-y1); 
+		double q = Math.abs(x2-y2);*/
+
+		//lesson 4.2
+		//1
+
+		/* int L = 15000;
+		int C = L/100;
+		System.out.println(C);*/
+
+		//доделать
+		/* int x = 32;
+		int x1 = x/10;
+		int x2 = x/x1;
+		System.out.println(x2);*/
+
+		//lesson 5
+		//tasks
+		//1
+
+		/* Scanner in = new Scanner(System.in);
+		System.out.println("Введите положительное число");
+		int n = in.nextInt();
+		if (n>0) {
+		System.out.println(n+1);
+		}
+		else{
+		System.out.println("Введено не положительное число");
+		}
+		*/
+
+		/*
+		int x= -4;
+		if (x>0) {
+		System.out.println(x+1);
+		}
+		else{
+		System.out.println(x);
+		}*/
+
+		//3st
+		/* int a = 4;
+		int b = -5;
+		int c = 6;
+		Scanner in = new Scanner(System.in);
+		int x =0;
+		if (a>0) {
+		++x;
+		}
+		if (b>0) {
+		++x;
+		}
+		if (c>0) {
+		++x;
+		}
+		System.out.println(x);*/
+
+		//2nd
+		/* int x= 0;
+		if (x>0) {
+		System.out.println(x+1);
+		if(x<0) {
+		System.out.println(x-2);
+		}
+		}
+		else{
+		System.out.println(10);
+		}*/
+
+		//5th
+		int a = 4;
+		int b = -5;
+		int c = 6; 
+		}
+		}
