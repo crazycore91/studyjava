49faa2a70d5e24064b5653e7b0ce79cdae2565bc
crazycore91 <karpenkoi@edi.su>
2017-04-12 23:41:23 +0300

    commitidea

--- src/dztask1stlesson/tasks1.java
+++ src/dztask1stlesson/tasks1.java
@@ -2,7 +2,7 @@ package dztask1stlesson;
 import java.util.Scanner;
 public class tasks1 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
 		/*int A= 5;
 		int B= 7;
@@ -14,7 +14,7 @@ public class tasks1 {
 		System.out.println(A);
 		System.out.println(B);*/
 
-		//7tsk
+        //7tsk
 		/*		int A= 4;
 				int B= 6;
 				int C= 8;
@@ -28,21 +28,21 @@ public class tasks1 {
 				System.out.println(C);*/
 
 
-		//task4.1.1
-		//1st
+        //task4.1.1
+        //1st
 
 			/*int A = -4;
 			boolean a1 = A >= 0;
 			System.out.println(a1);*/
 
-		//2st
+        //2st
 
 				/*int A = 4;
 				boolean a1 = (A%2)!=0;
 				System.out.println(a1);*/
 
-		//3st
-		//Даны три целых числа: A, B, C. Проверить истинность высказывания: «Справедливо двойное неравенство A < B < C».
+        //3st
+        //Даны три целых числа: A, B, C. Проверить истинность высказывания: «Справедливо двойное неравенство A < B < C».
 		/*int A= 21;
 		int B= 22;
 		int C= 30;
@@ -50,7 +50,7 @@ public class tasks1 {
 		System.out.println(a1);*/
 
 
-		//4d
+        //4d
 		/*int A= 13;
 		int B= -5;
 		int C= 8;
@@ -58,7 +58,7 @@ public class tasks1 {
 		System.out.println(a1);*/
 
 
-		//5
+        //5
 		/*int A= 3;
 		int B= -7;
 		int C= 8;
@@ -67,14 +67,14 @@ public class tasks1 {
 		((A < 0) && (B < 0 ) && (C > 0));
 		System.out.println(a1);*/
 
-		//6
+        //6
 
 		/* int A= 13;
 		boolean a1 = ((A>99) && (A<1000)) && (A%2)!=0;
 		System.out.println(a1);
 		*/
 
-		//7 проверить -Даны числа x, y, x1, y1, x2, y2. Проверить истинность высказывания:«Точка с координатами (x, y) лежит внутри прямоугольника, левая верхняя вершина которого имеет координаты (x1, y1), правая нижняя — (x2, y2), а стороны параллельны координатным осям».
+        //7 проверить -Даны числа x, y, x1, y1, x2, y2. Проверить истинность высказывания:«Точка с координатами (x, y) лежит внутри прямоугольника, левая верхняя вершина которого имеет координаты (x1, y1), правая нижняя — (x2, y2), а стороны параллельны координатным осям».
 
 
 	/*	int x = 20;
@@ -86,7 +86,7 @@ public class tasks1 {
 		boolean z = (x1>x)&&(x2>x)&&(y1>y)&&(y2>y);
 		System.out.println(z); */
 
-		//8 - проверить Даны целые числа a, b, c. Проверить истинность высказывания: «Существует треугольник со сторонами a, b, c».
+        //8 - проверить Даны целые числа a, b, c. Проверить истинность высказывания: «Существует треугольник со сторонами a, b, c».
 
      /*	int A= 13;
 		int B= 5;
@@ -94,41 +94,41 @@ public class tasks1 {
 		boolean z = ((A+B-C)>0) && ((B+C-A)>0) &&((C+A-B)>0);
 		System.out.println(z);*/
 
-		//9 - проверить Даны координаты поля шахматной доски x, y (целые числа, лежащие в диапазоне 1–8).
-		//Учитывая, что левое нижнее поле доски (1, 1) является черным, проверить истинность высказывания: «Данное поле является белым»
+        //9 - проверить Даны координаты поля шахматной доски x, y (целые числа, лежащие в диапазоне 1–8).
+        //Учитывая, что левое нижнее поле доски (1, 1) является черным, проверить истинность высказывания: «Данное поле является белым»
 
 		/*	int x = 4;
 		int y = 3;
 		boolean c = ((x%3)!=0) && ((y%3)!=0);
 		System.out.println(c); */
 
-		//10 проверить Даны координаты двух различных полей шахматной доски x1, y1, x2, y2 (целые числа, лежащие в диапазоне 1–8).
-		//Проверить истинность высказывания: «Ферзь за один ход может перейти с одного поля на другое».
+        //10 проверить Даны координаты двух различных полей шахматной доски x1, y1, x2, y2 (целые числа, лежащие в диапазоне 1–8).
+        //Проверить истинность высказывания: «Ферзь за один ход может перейти с одного поля на другое».
 
-		/* int x1 = 3;
-		int y1 = 4;
-		int x2 = 5;
-		int y2 = 6;
-		boolean c = ((x1+y1)>8)&&((x2+y2)>8);
-		System.out.println(c); */
+      /*  int x1 = 3;
+        int y1 = 4;
+        int x2 = 5;
+        int y2 = 6;
+        boolean c = ((x1 + y1) > 8) && ((x2 + y2) > 8);
+        System.out.println(c);*/
 
 
-		//11
-		// Даны два числа А и В. Поменяйте их местами не используя дополнительную переменную и арифметические операции
+        //11
+        // Даны два числа А и В. Поменяйте их местами не используя дополнительную переменную и арифметические операции
 		/*int A = 5;
 		int B = 7;
 		A=A+B-(B=A);
 		System.out.println(A);
 		System.out.println(B);*/
 
-		//lesson 4.2
-		//1
+        //lesson 4.2
+        //1
 
 		/* int L = 15000;
 		int C = L/100;
 		System.out.println(C);*/
 
-		// 2 Дано двузначное число. Вывести число, полученное при перестановке цифр исходного числа.
+        // 2 Дано двузначное число. Вывести число, полученное при перестановке цифр исходного числа.
 
 		/*int x = 47;
 		int x1 = x/10;
@@ -136,7 +136,7 @@ public class tasks1 {
 		System.out.println(x2+ ""+ x1);*/
 
 
-		// 3 Дано трехзначное число. Вывести число, полученное при прочтении исходного числа справа налево.
+        // 3 Дано трехзначное число. Вывести число, полученное при прочтении исходного числа справа налево.
 		/*int x = 367;
 		int x1 = x/100;
 		int x2 = x%100/10;
@@ -144,9 +144,9 @@ public class tasks1 {
 		System.out.println(x3+""+x2+""+x1);*/
 
 
-		// 4.2.4 Даны целые положительные числа A, B, C. На прямоугольнике раз мера A × B размещено максимально возможное количество квадратов со стороной C (без наложений).
-		// Найти количество квадратов, размещенных на прямоугольнике, а также площадь незанятой части прямоугольника.
-		// (необходимо пояснение)
+        // 4.2.4 Даны целые положительные числа A, B, C. На прямоугольнике раз мера A × B размещено максимально возможное количество квадратов со стороной C (без наложений).
+        // Найти количество квадратов, размещенных на прямоугольнике, а также площадь незанятой части прямоугольника.
+        // (необходимо пояснение)
 		/*int a = 9;
 		int b = 8;
 		int c = 22;
@@ -156,10 +156,10 @@ public class tasks1 {
 		System.out.println("ploscghad' = "+Z);*/
 
 
-		//lesson 5
-		//tasks
+        //lesson 5
+        //tasks
 
-		//1
+        //1
 		/*
 		Scanner in = new Scanner(System.in);
 		System.out.println("Введите положительное число");
@@ -173,7 +173,7 @@ public class tasks1 {
 		*/
 
 
-		//3st
+        //3st
 		/*Scanner in = new Scanner(System.in);
 		System.out.println("Введите первое число:");
 		int a = in.nextInt();
@@ -193,7 +193,7 @@ public class tasks1 {
 		}
 		System.out.println("Колличество положительных чисел = " +x);*/
 
-		//2nd
+        //2nd
 		/*Scanner in = new Scanner(System.in);
 		System.out.println("Введите целое число:");
 		int x = in.nextInt();
@@ -209,7 +209,7 @@ public class tasks1 {
 		System.out.println("Программа закончила свое выполнение");*/
 
 
-		//4th
+        //4th
 		/*Scanner in = new Scanner(System.in);
 		System.out.println("Введите первое число:");
 		int a = in.nextInt();
@@ -240,7 +240,7 @@ public class tasks1 {
 		System.out.println("Колличество положительных чисел = " +x);
 		System.out.println("Колличество отрицательныз чисел = " +y);*/
 
-		//5 task
+        //5 task
 		/* Scanner in = new Scanner(System.in);
 		System.out.println("Введите первое число:");
 		int a = in.nextInt();
@@ -260,7 +260,7 @@ public class tasks1 {
 		}
 		System.out.println("Наименьшее число = " +x);*/
 
-		//6 task 1st method
+        //6 task 1st method
 		/*Scanner in = new Scanner(System.in);
 		System.out.println("Введите номер года:");
 		int a = in.nextInt();
@@ -277,7 +277,7 @@ public class tasks1 {
 		}
 		System.out.println("Номер года "+a+" имеет "+ d + " дней в году" );*/
 
-		//1st method
+        //1st method
 		/* Scanner in = new Scanner(System.in);
 		System.out.println("Введите номер года:");
 		int a = in.nextInt();
@@ -294,7 +294,7 @@ public class tasks1 {
 		System.out.println("Номер года "+a+" имеет "+ 366 + " дней в году" );
 		}
 		*/
-		// 7task
+        // 7task
 		/* Scanner in = new Scanner(System.in);
 		System.out.println("Введите любое целое число от 1 до 999:");
 		int a = in.nextInt();
@@ -311,7 +311,7 @@ public class tasks1 {
 		System.out.println(a+" - данное число, является четным, трехзначным");
 		}*/
 
-		// 8task
+        // 8task
 		/*Scanner in = new Scanner(System.in);
 		System.out.println("Введите день недели, используя число от 1 до 7:");
 		int a = in.nextInt();
@@ -326,7 +326,7 @@ public class tasks1 {
 		default:System.out.println("Введено число не принадлежащее диапазону от 1 до 7"); break;
 		}*/
 
-		// 9task
+        // 9task
 		/* Scanner in = new Scanner(System.in);
 		System.out.println("Введите число от 1 до 5:");
 		int K = in.nextInt();
@@ -339,7 +339,7 @@ public class tasks1 {
 		default:System.out.println("Введено число не принадлежащее диапазону от 1 до 5"); break;
 		}*/
 
-		// 10 task
+        // 10 task
 
 		/* Scanner in = new Scanner(System.in);
 		System.out.println("Введите число от 1 до 5:");
@@ -374,7 +374,7 @@ public class tasks1 {
 		System.out.println(C);*/
 
 
-		//less 6.02 1st task 1st method
+        //less 6.02 1st task 1st method
 
 		/*Scanner in = new Scanner(System.in);
 		System.out.println("Введите число A:");
@@ -386,7 +386,7 @@ public class tasks1 {
 		}
 		System.out.println("Колличество чисел = " + (B-A));*/
 
-		//2st method
+        //2st method
 		/* Scanner in = new Scanner(System.in);
 		System.out.println("Введите число A:");
 		int A = in.nextInt();
@@ -399,7 +399,7 @@ public class tasks1 {
 		while (s<B);
 		System.out.println("Колличество чисел = " + (B-A));*/
 
-		//2task 
+        //2task
 /*		 Scanner in = new Scanner(System.in);
 		System.out.println("Введите число A:");
 		int A = in.nextInt();
@@ -411,7 +411,7 @@ public class tasks1 {
 		}
 		System.out.println("Колличество чисел = " + (B-A));
 */
-		//3task на проверку
+        //3task на проверку
 	/*	 Scanner in = new Scanner(System.in);
 		System.out.println("Введите число A:");
 		int A = in.nextInt();
@@ -422,8 +422,8 @@ public class tasks1 {
 		System.out.println(A-s);s--;
 		}
 		System.out.println("Колличество чисел = " + (A-B));*/
-		
-		//4task на проверку
+
+        //4task на проверку
 		/* Дано целое число N (> 3). Если оно является степенью числа 3, то вывести True, если не
           является — вывести False. */
 		
@@ -443,20 +443,35 @@ public class tasks1 {
 		P процентов от имеющейся суммы (P — вещественное число, 0 < P < 25). По данному P
 		определить, через сколько месяцев размер вклада превысит 1100 руб., и вывести найденное
 		количество месяцев K (целое число) и итоговый размер вклада S (вещественное число).*/
-		
-		Scanner in = new Scanner(System.in);
+        // НЕ РЕШЕНА, УЗНАТЬ И ПОНЯТЬ КАК
+		/*Scanner in = new Scanner(System.in);
 		System.out.println("Введите число 0 < P < 25:");
 		int P = in.nextInt();
 		int a = 1000;
-		int S;
-		int K;
-		int i = 1;
-		while(i<12) {
-		    System.out.println(a+(a*P)/100);a++;
-
+		int d = (a*P)/100;
+        int S=a;
+		do {
+            System.out.println(S);
+            S+=d;
+        }while((S)<1100);
         }
-		
-		//less 1_7 1st
+*/
+
+       /* 6. Дано целое число N (> 0). Используя операции деления нацело и взятия остатка от деления,
+         найти число, полученное при прочтении числа N справа налево.*/
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число N(> 0):");
+        int N = in.nextInt();
+        int n =0;
+        while(N>0){
+            n*=10;
+            n+=N%10;
+            N/=10;
+            System.out.print(N);
+        }
+
+        //less 1_7 1st
 		/*2. Дано целое число N (> 0). Найти произведение
 		N! = 1·2·…·N
 		(N–факториал). Чтобы избежать целочисленного переполнения, вычислять это произведение
@@ -483,5 +498,6 @@ public class tasks1 {
 		x -= i;
 		}
 		System.out.println(x);*/
-		}
+    }
 }
+
